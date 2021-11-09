818625647c26df3ae93fc3ca298b7d1d2666df2f
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-11-09 08:46:59 -0500

    first commit

---
+++ src/main/java/Triangles.java
@@ -0,0 +1,17 @@
+public class Triangles {
+    public String getTriangle(int numberOfRows) {
+        return null;
+    }
+
+    public String getRow(int numberOfStars) {
+        return null;
+    }
+
+    public String getSmallTriangle() {
+        return null;
+    }
+
+    public String getLargeTriangle() {
+        return null;
+    }
+}
---
+++ src/test/java/TrianglesTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TrianglesTest {
+
+    private Triangles triangles;
+
+    @BeforeEach
+    public void setUp(){
+        triangles = new Triangles();
+    }
+
+    @Test
+    public void getRow() {
+        String expected = "********************";
+        int width = 20;
+        String actual = triangles.getRow(width);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void getTriangleTest1() {
+        String expected =
+                "*\n" +
+                        "**\n" +
+                        "***\n" +
+                        "****\n" +
+                        "*****\n" +
+                        "******\n" +
+                        "*******\n" +
+                        "********\n" +
+                        "*********\n";
+        String actual = triangles.getTriangle(10);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTriangleTest2() {
+        String expected =
+                "*\n" +
+                        "**\n" +
+                        "***\n" +
+                        "****\n";
+        String actual = triangles.getTriangle(5);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetLargeTriangle() {
+        String expected =
+                "*\n" +
+                        "**\n" +
+                        "***\n" +
+                        "****\n" +
+                        "*****\n" +
+                        "******\n" +
+                        "*******\n" +
+                        "********\n" +
+                        "*********\n";
+        String actual = triangles.getLargeTriangle();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testGetSmallTriangle() {
+        String expected =
+                "*\n" +
+                        "**\n" +
+                        "***\n" +
+                        "****\n";
+        String actual = triangles.getSmallTriangle();
+        Assertions.assertEquals(expected, actual);
+    }
+}
